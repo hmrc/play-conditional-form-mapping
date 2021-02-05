26ca2290a92ce372193c4fb4ea05e5ba6474df7b
Habeeb <h.alliowe@gmail.com>
2021-02-05 13:32:13 +0000

    VTCCA-3934: set play 2.7 as default

--- project/PlayCrossCompilation.scala
+++ project/PlayCrossCompilation.scala
@@ -1,4 +1,4 @@
 import uk.gov.hmrc.playcrosscompilation.AbstractPlayCrossCompilation
-import uk.gov.hmrc.playcrosscompilation.PlayVersion.Play26
+import uk.gov.hmrc.playcrosscompilation.PlayVersion.Play27
 
-object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play26)
+object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play27)
