ca047459b43086f0ca7face9c9f9c4ef575cf080
Peter Perhac <PeterPerhac@users.noreply.github.com>
2022-02-01 11:51:29 +0000

    VTCCA-4620 remove references to old versions of play framework (#13)

--- project/PlayCrossCompilation.scala
+++ project/PlayCrossCompilation.scala
@@ -1,4 +1,4 @@
 import uk.gov.hmrc.playcrosscompilation.AbstractPlayCrossCompilation
-import uk.gov.hmrc.playcrosscompilation.PlayVersion.Play27
+import uk.gov.hmrc.playcrosscompilation.PlayVersion.Play28
 
-object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play27)
+object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play28)
